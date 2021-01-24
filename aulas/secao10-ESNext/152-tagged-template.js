9ed1ec968c6e4ab606afd2915fffe4a942822592
Hugo Brandao <hugobrandao.dev@gmail.com>
2021-01-24 19:28:22 -0400

    Adicionado um exemplo simples de Tagged Template, que separa caracteres regulares de variáveis interpoladas em uma string.

---
+++ aulas/secao10-ESNext/152-tagged-template.js
@@ -0,0 +1,10 @@
+function separarCaracteresValores(caracteres, ...valores) {
+    console.log(caracteres)
+    console.log(valores)
+    return 'Sucesso na separação'
+}
+
+const nome = 'Dinorá'
+const idade = 41
+
+console.log(separarCaracteresValores `${nome} tem ${idade} anos de idade.`)
