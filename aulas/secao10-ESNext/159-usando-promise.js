ce8dfbadb7449a7ced2b91c84da9e434a28441d0
Hugo Brandao <hugobrandao.dev@gmail.com>
2021-01-28 00:29:02 -0400

    Refarização parcial do código da aula anterior (158), desta vez utilizando alguns conceitos de Promises.

---
+++ aulas/secao10-ESNext/159-usando-promise.js
@@ -0,0 +1,34 @@
+const http = require('http')
+
+function fazerRequisicaoAlunos(letra) {
+    let letraMaiuscula = letra.toUpperCase()
+    let url = `http://files.cod3r.com.br/curso-js/turma${letraMaiuscula}.json`
+
+    return new Promise((resolve, reject) => {
+        http.get(url, requisicao => {
+            let resposta = ''
+    
+            requisicao.on('data', dados => {
+                resposta += dados
+            })
+    
+            requisicao.on('end', _ => {
+                try {
+                    resolve(JSON.parse(resposta))
+                } catch (error) {
+                    reject(error)
+                }
+            })
+        })
+    })
+}
+
+let classes = ['a', 'b', 'c', 'd']
+
+Promise.all(classes.map(valor => {
+    return fazerRequisicaoAlunos(valor)
+}))
+    .then(turmas => [].concat(...turmas))
+    .then(alunos => alunos.map(aluno => aluno.nome))
+    .then(nomes => console.log(nomes))
+    .catch(error => console.log(error.message))
