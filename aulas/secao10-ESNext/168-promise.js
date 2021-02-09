9b691f1c83389ca37127d1d40e8f0a1f86b8c12b
Hugo Brandao <hugobrandao.dev@gmail.com>
2021-02-09 13:53:30 -0400

    Adicionado mais um exemplo de promise, não só utilizando o then, mas o catch também, para que possamos tratar os erros. O tratamento de erros dentro do próprio then também está presente.

---
+++ aulas/secao10-ESNext/168-promise.js
@@ -0,0 +1,31 @@
+function funcionarOuNao(valor, chanceErro) {
+    return new Promise((resolve, reject) => {
+        try {
+            if (Math.random() > chanceErro) {
+                resolve(valor)
+            } else {
+                reject('Ocorreu um erro!') // Esse "erro" é referente ao PARÂMETRO informado pelo usuário.
+            }
+        } catch (error) {
+            reject(erro) // Esse erro é caso a FUNÇÃO dê problema.
+        }
+    })
+}
+
+funcionarOuNao('Testando...', 0.5)
+    .then(valor => console.log(`Valor: ${valor}`))
+    .then(
+        valor => console.log(valor),
+        /*
+        error => { // Embora específico, será chamado em qualquer erro.
+            console.log(`Erro específico: ${error}`)
+        }
+        */
+    )
+    .then(console.log('Quase fim!'))
+    .catch(mensagemErro => console.log(`Erro geral: ${mensagemErro}`))
+    .then(() => console.log('Fim!')) // Não há dados a partir do catch
+
+/*
+    
+*/
